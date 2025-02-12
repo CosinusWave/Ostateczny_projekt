30b11a2643d66230058f59e0d6cbc6049cd41efa
CosinusWave <kkulawik@st.swps.edu.pl>
2025-02-12 21:09:28 +0100

    Delete DjangoProject1 directory

--- DjangoProject1/swps-project/remiza/remizotron9000/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import FireFighter, FireTruck, FirefighterInSquad, Squad
-
-admin.site.register(FireFighter)
-admin.site.register(FireTruck)
-admin.site.register(FirefighterInSquad)
-admin.site.register(Squad)
